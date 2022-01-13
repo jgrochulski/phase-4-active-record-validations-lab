36bf57dec534b71458f8bd2f3506b4854c986d24
jan <janek.grochulski@gmail.com>
2022-01-13 11:41:30 -0600

    fin w/ regex

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  validate :is_clickbait, on: :create
+  
+  def is_clickbait
+    if !(/top [0-9]|secret|won't believe|guess/i =~ self.title)
+      errors.add(:title, "must contain clickbait")
+    end
+  end
+
 end
